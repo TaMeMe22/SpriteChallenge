35b9b397e5538e553a79e7ea88e3808a912ee2f3
TaMeMe22 <71111393+TaMeMe22@users.noreply.github.com>
2020-11-05 20:57:46 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+function myFunction(imgs) {
+  var expandImg = document.getElementById("expandedImg");
+  var imgText = document.getElementById("imgtext");
+  expandImg.src = imgs.src;
+  imgText.innerHTML = imgs.alt;
+  expandImg.parentElement.style.display = "block";
+}
+
+
+function openForm() {
+  document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+}
+
+function myFunction() {
+  document.getElementById("myForm").method = "post";
+  
+}
+  
+
